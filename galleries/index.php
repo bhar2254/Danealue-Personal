9b45e80bcc7421101150b18bd287a9fc3583bba6
Blaine Harper <bhar2254@student.liberty.k12.mo.us>
2015-05-11 09:53:09 -0500

    FRC head/footer

--- galleries/index.php
+++ galleries/index.php
@@ -3,7 +3,7 @@ $pageTitle = $_GET["title"];
 $prependPath = "/var/www/danealue";
 $prependCSSPath = null;
 
-include($prependPath."/base/header.html");
+include($prependPath."/base/head.html");
 include($prependPath."/galleries/".$_GET["gallery"].".php");
 include($prependPath."/base/footer.html");
 ?>
